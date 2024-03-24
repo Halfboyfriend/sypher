57230902cbfe4befcb312aa1b7f7deb4b4b96f67
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2024-03-24 10:15:56 +0100

    local-privateKey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const[dt, setDt] = useState("")
+  const[key, setKey] = useState("")
+
+  useEffect(() => {
+    // localStorage.removeItem("pKey");
+    getPvKey();
+  }, []);
+
+
+  function setPvKey(e) {
+    e.preventDefault();
+    localStorage.setItem("pKey", key);
+    window.location.reload();
+    
+    console.log(key)
+  }
+
+
+
+  function getPvKey() {
+   const data = localStorage.getItem("pKey");
+   setDt(data);
+   console.log(data)
+  }
+
+  return <div className="App">
+
+    {dt ? <div>
+      <p>PrivateKey: {dt}</p>
+    </div> :
+    
+    <div>
+      <form onSubmit={setPvKey}>
+        <input type="text" placeholder="input key" onChange={(e) => setKey(e.target.value)} required />
+        <button>Submit</button>
+      </form>
     </div>
-  );
+    
+    }
+    
+  </div>;
 }
 
 export default App;
